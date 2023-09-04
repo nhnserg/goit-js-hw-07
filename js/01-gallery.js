36bdc14bea5b5b7867961078e72316c57d7cc07d
nhnserg <nhnsergfr@gmail.com>
2023-09-04 18:03:48 +0100

    new soluthion

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import * as basicLightbox from "basiclightbox";
+import { galleryItems } from "./gallery-items.js";
+const gallery = document.querySelector(".gallery", "./gallery-items.js");
 
+gallery.addEventListener("click", (event) => {
+  if (event.target.nodeName !== "IMG") return;
+
+  event.preventDefault();
+  const source = event.target.src;
+  const instance = basicLightbox.create(`
+  <img src="${source}" alt="Image description">
+`);
+
+  instance.show();
+
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape" && lightbox.visible()) {
+      lightbox.close();
+    }
+  });
+});
 console.log(galleryItems);
