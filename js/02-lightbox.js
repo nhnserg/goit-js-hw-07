27ac9dd7dcccbcd3676939dbfb9c3e7e6028a146
nhnserg <nhnsergfr@gmail.com>
2023-09-05 21:26:37 +0100

    finish second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+document.addEventListener("DOMContentLoaded", () => {
+  const gallery = document.querySelector(".gallery");
+  // функція для створення HTML - структури галереї у вигляді рядка
+  function createGalleryHTML(items) {
+    return items
+      .map(
+        (item) => `<li class="gallery__item">
+          <a class="gallery__link" 
+          href="${item.original}">
+        <img class="gallery__image" 
+        src="${item.preview}" 
+        alt="${item.description}" />
+          </a>
+       </li>`
+      )
+      .join("");
+  }
+  // створення HTML- структури галереї
+  gallery.innerHTML = createGalleryHTML(galleryItems);
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+});
